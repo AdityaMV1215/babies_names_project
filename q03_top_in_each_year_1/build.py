1bb156f891325fdc7296e7d2231a5f0bca9b75f8
AdityaMV1215 <aditya.vadhavkar12@gmail.com>
2018-04-17 13:34:53 +0000

    Done

--- q03_top_in_each_year_1/build.py
+++ q03_top_in_each_year_1/build.py
@@ -1,9 +1,18 @@
 
 import pandas as pd
-
+import numpy as np
+from collections import defaultdict
 path = 'data/babies_name.csv'
 data = pd.read_csv(path,names=['Name', 'Gender', 'Count', 'Year'])
 
-def q03_top_in_each_year_1():
+def q03_top_in_each_year_1(data):
     "write your solution here"
+    dict3 = defaultdict(dict)
+    unique_years = np.unique(data['Year'])
+    for val in unique_years:
+        temp = data[data['Year'] == val]
+        for x in temp.index.values:
+            dict3[val][temp.loc[x,'Name']] = temp.loc[x,'Count']
+    return dict3
 
+q03_top_in_each_year_1(data)
