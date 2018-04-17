3eeb076d0c5dec2607450f4b3d1177a1e99611e7
AdityaMV1215 <aditya.vadhavkar12@gmail.com>
2018-04-17 12:44:55 +0000

    Done

--- q01_create_dict/build.py
+++ q01_create_dict/build.py
@@ -3,6 +3,9 @@ import pandas as pd
 path = 'data/babies_name.csv'
 data = pd.read_csv(path,names=['Name', 'Gender', 'Count', 'Year'])
 
-def q01_create_dict():
-    "write your solution here"
+def q01_create_dict(data):
+    dict1 = {}
+    for x in data.index.values:
+        dict1[data.loc[x,'Name']] = data.loc[x,'Count']
 
+    return dict1
